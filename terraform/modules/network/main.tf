63f35bfa6e1545b745a3927c97b350d38929dcc6
phamdevctpmq <phamminhduc0865713395@gmail.com>
2026-05-28 01:07:13 +0700

    add terraform network

---
+++ terraform/modules/network/main.tf
@@ -0,0 +1,78 @@
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
+
+resource "azurerm_virtual_network" "hub" {
+  name                = var.hub_vnet_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.0.0.0/16"]
+}
+
+
+resource "azurerm_virtual_network" "spoke" {
+  name                = var.spoke_vnet_name
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  address_space       = ["10.1.0.0/16"]
+}
+
+resource "azurerm_virtual_network_peering" "hub_spoke" {
+  name                      = var.hub_spoke
+  resource_group_name       = azurerm_resource_group.main.name
+  virtual_network_name      = azurerm_virtual_network.hub.name
+  remote_virtual_network_id = azurerm_virtual_network.spoke.id
+  allow_forwarded_traffic      = true
+
+}
+resource "azurerm_virtual_network_peering" "spoke_hub" {
+  name                      = var.spoke_hub
+  resource_group_name       = azurerm_resource_group.main.name
+  virtual_network_name      = azurerm_virtual_network.spoke.name
+  remote_virtual_network_id = azurerm_virtual_network.hub.id
+  allow_forwarded_traffic      = true
+
+}
+
+resource "azurerm_subnet" "system_pool_subnet" {
+  name                 = var.system_pool_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+resource "azurerm_subnet" "app_pool_subnet" {
+  name                 = var.app_pool_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.1.2.0/24"]
+}
+resource "azurerm_subnet" "cron_job_pool_subnet" {
+  name                 = var.cron_job_pool_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.1.3.0/24"]
+}
+resource "azurerm_subnet" "kafka_subnet" {
+  name                 = var.kafka_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.hub.name
+  address_prefixes     = ["10.1.4.0/24"]
+}
+resource "azurerm_subnet" "database" {
+  name                 = var.database_subnet_name
+  resource_group_name  = azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.spoke.name
+  address_prefixes     = ["10.1.5.0/24"]
+
+  delegation {
+    name = "postgresql-delegation"
+    service_delegation {
+      name = "Microsoft.DBforPostgreSQL/flexibleServers"
+      actions = [
+        "Microsoft.Network/virtualNetworks/subnets/join/action"
+      ]
+    }
+  }
+}
---
+++ terraform/modules/network/variables.tf
@@ -0,0 +1,56 @@
+variable "resource_group_name" {
+    description = "Name of the resource group to create"
+    type        = string
+    default     = "resource-group-main"
+}
+variable "location" {
+    description = "Azure region where resources will be created"
+    type        = string
+    default     = "southeastasia"
+}
+variable "hub_vnet_name" {
+    description = "Name of the hub virtual network"
+    type        = string
+    default     = "hub-vnet"
+}
+variable "spoke_vnet_name" {
+    description = "Name of the spoke virtual network"
+    type        = string
+    default     = "spoke-vnet"
+}
+variable "hub_spoke" {
+    description = "Name of the peering from hub to spoke"   
+    type        = string
+    default     = "hub-spoke"
+}
+variable "spoke_hub" {
+    description = "Name of the peering from spoke to hub"
+    type        = string
+    default     = "spoke-hub"
+}                       
+variable "system_pool_subnet_name" {
+    description = "Name of the system pool subnet"
+    type        = string
+    default     = "system-pool-subnet"
+}
+variable "app_pool_subnet_name" {
+    description = "Name of the app pool subnet"
+    type        = string
+    default     = "app-pool-subnet"
+}
+variable "cron_job_pool_subnet_name" {
+    description = "Name of the cron job pool subnet"
+    type        = string
+    default     = "cron-job-pool-subnet"
+}
+variable "kafka_subnet_name" {
+    description = "Name of the Kafka subnet"
+    type        = string
+    default     = "kafka-subnet"
+}
+variable "database_subnet_name" {
+    description = "Name of the database subnet"
+    type        = string
+    default     = "database-subnet"
+}
+
